25ed319464cdaed027bf0af2b66fd18334702a6f
rethfupy <solamdev@outlook.com>
2025-07-03 16:36:22 +0300

    Modified component

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
     data() {
         return {
             questionsAnswered: 0,
+            totalCorrect: 0,
             questions: [
                 {
                     q: 'What is (8 - 2) * 2?', 
@@ -91,13 +92,19 @@ export default {
                 }
             ]
         }
+    },
+    methods: {
+        questionAnswered(is_correct) {
+            if (is_correct) { this.totalCorrect++; }
+            this.questionsAnswered++;
+        }
     }
 }
 </script>
 
 <template>
     <div class="ctr">
-        <questions v-if="questionsAnswered < questions.length" :questions="questions" :questionsAnswered="questionsAnswered" />
+        <questions v-if="questionsAnswered < questions.length" :questions="questions" :questionsAnswered="questionsAnswered" @question-answered="questionAnswered"/>
         <result v-else />
         <button type="button" class="reset-btn">Reset</button>
     </div>
